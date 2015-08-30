4b22b5421e52912505fe8faf68324b00c066741f
BritanyannCopperfield <britanyann@imperialestates.biz>
2015-08-30 18:17:14 -0400

    Clean up the CombatModule
    - Some Aesthetics
    - Remove commented out code that isn't used
    - Adding a using reference so the console messages when uncommented will work

--- Vision/Modules/Avatar/CombatModule/CombatModule.cs
+++ Vision/Modules/Avatar/CombatModule/CombatModule.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Contributors, http://vision-sim.org/, http://aurora-sim.org
+ * Copyright (c) Contributors, http://vision-sim.org/, http://whitecore-sim.org/, http://aurora-sim.org/, http://opensimulator.org
  * See CONTRIBUTORS.TXT for a full list of copyright holders.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,6 +30,7 @@ using System.Collections.Generic;
 using System.Timers;
 using Nini.Config;
 using OpenMetaverse;
+using Vision.Framework.ConsoleFramework;
 using Vision.Framework.Modules;
 using Vision.Framework.Physics;
 using Vision.Framework.PresenceInfo;
@@ -328,46 +329,8 @@ namespace Vision.Modules.Combat
 
             public void PhysicsActor_OnCollisionUpdate(EventArgs e)
             {
-                /*if (HasLeftCombat)
-                    return;
-                */
                 if (e == null)
                     return;
-
-                /*CollisionEventUpdate collisionData = (CollisionEventUpdate) e;
-                Dictionary<uint, ContactPoint> coldata = collisionData.m_objCollisionList;
-
-                UUID killerObj = UUID.Zero;
-                foreach (uint localid in coldata.Keys)
-                {
-                    ISceneChildEntity part = m_part.Scene.GetSceneObjectPart(localid);
-                    if (part != null && part.ParentEntity.Damage != -1.0f)
-                    {
-                        if (part.ParentEntity.Damage > MaximumDamageToInflict)
-                            part.ParentEntity.Damage = MaximumDamageToInflict;
-
-                        Health -= part.ParentEntity.Damage;
-                        if (Health <= 0.0f)
-                            killerObj = part.UUID;
-                    }
-                    else
-                    {
-                        float Z = Math.Abs(m_part.Velocity.Z);
-                        if (coldata[localid].PenetrationDepth >= 0.05f)
-                            Health -= coldata[localid].PenetrationDepth*Z;
-                    }
-
-                    //Regenerate health (this is approx 1 sec)
-                    if ((int) (Health + 0.0625) <= m_combatModule.MaximumHealth)
-                        Health += 0.0625f;
-
-                    if (Health > m_combatModule.MaximumHealth)
-                        Health = m_combatModule.MaximumHealth;
-                }
-                if (Health <= 0)
-                {
-                    Die(killerObj);
-                }*/
             }
 
             public void LeaveCombat()
@@ -450,7 +413,6 @@ namespace Vision.Modules.Combat
             string m_Team = "No Team";
             CombatModule m_combatModule;
             float m_health = 100f;
-            //Dictionary<string, float> GenericStats = new Dictionary<string, float>();
 
             public float Health
             {
@@ -599,7 +561,7 @@ namespace Vision.Modules.Combat
 
                     if (Health <= 0 && killingAvatar == null)
                         killingAvatar = otherAvatar;
-                    //MainConsole.Instance.Debug("[AVATAR]: Collision with localid: " + localid.ToString() + " at depth: " + coldata[localid].ToString());
+                    //MainConsole.Instance.Debug("[Avatar]: Collision with localid: " + localid.ToString() + " at depth: " + coldata[localid].ToString());
                 }
 
                 if (starthealth != Health)
