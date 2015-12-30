002c42a9f90758ae585b113b4de349e4201fcebe
BritanyannCopperfield <britanyann@imperialestates.biz>
2015-12-30 08:04:45 -0500

    Quick fix

--- Vision/Modules/Web/html/admin/transactions.cs
+++ Vision/Modules/Web/html/admin/transactions.cs
@@ -25,13 +25,13 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-using Vision.Framework.Servers.HttpServer.Implementation;
-using System.Collections.Generic;
-using Vision.Framework.Modules;
-using Vision.Framework.Utilities;
 using System;
+using System.Collections.Generic;
 using OpenMetaverse;
+using Vision.Framework.Modules;
+using Vision.Framework.Servers.HttpServer.Implementation;
 using Vision.Framework.Services;
+using Vision.Framework.Utilities;
 
 namespace Vision.Modules.Web
 {
