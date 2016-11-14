0653da1120b5be9f95b58eacead7f06576838187
BritanyannCopperfield <britanyann@imperialestates.biz>
2016-11-14 13:57:02 -0500

    osVolumeDetect
    - Implement osVolumeDetect

--- Vision/ScriptEngine/VisionScript/APIs/OSSL/OS_Api.cs
+++ Vision/ScriptEngine/VisionScript/APIs/OSSL/OS_Api.cs
@@ -3762,5 +3762,16 @@ namespace Vision.ScriptEngine.VisionScript.APIs
                 World.EventManager.TriggerObjectDeGrab(child.ParentEntity.RootChild, child, sp.ControllingClient, touchArgs);                             
             }
         }
+
+         // This may still not very usefull, detector is lost on rez, restarts, etc
+         public void osVolumeDetect(int detect)
+         {
+             m_host.AddScriptLPS(1);
+
+             if (m_host.ParentGroup == null || m_host.ParentGroup.IsDeleted || m_host.ParentGroup.IsAttachment)
+                 return;
+
+             m_host.ScriptSetVolumeDetect(detect != 0);
+         }
     }
 }
